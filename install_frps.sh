9a731dd7e4825f6e91dbd9d4a89f49086248f19e
1769862201 <1769862201@qq.com>
2023-06-21 12:13:26 +0900

    install_frps.sh

--- install_frps.sh
+++ install_frps.sh
@@ -5,21 +5,17 @@
 wget -O frp_0.49.0_linux_amd64.tar.gz https://github.com/fatedier/frp/releases/download/v0.49.0/frp_0.49.0_linux_amd64.tar.gz
 # 解压 frp
 tar -xzvf frp_0.49.0_linux_amd64.tar.gz
-cd frp_0.46.1_linux_arm64/
+cd frp_0.49.0_linux_amd64/
 # 配置 frps 设置
+bind_port="7000"
+token="freefrp.net"
 read -ep "请输入FRPS监听端口: （7000）" bind_port &&
 read -ep "请输入FRPS Token: （freefrp.net）" token
-if [[ -z "${bind_port}" ]]; then
-	bind_port=7000
-fi
-if [[ -z "${token}" ]]; then
-	token=freefrp.net
-fi
 # 配置 frps
 cat << EOF > ./frps.ini
 [common]
-bind_port = ${bind_port}
-token = ${token}
+bind_port = $(bind_port)
+token = $(token)
 EOF
 # 启动 frps
-# nohup ./frps -c ./frpa.ini > run.log 2>&1 &
+nohup ./frps -c ./frpa.ini > run.log 2>&1 &
